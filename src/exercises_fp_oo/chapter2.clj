353163c9b2736c40b07292f4922889a899f8fd82
Manuel Alonso <malonso@unience.com>
2013-02-12 09:19:19 +0100

    add and make data like objects

--- src/exercises_fp_oo/chapter2.clj
+++ src/exercises_fp_oo/chapter2.clj
@@ -10,5 +10,19 @@
 
  (def class_of :__class_symbol__)
 
+ (def make 
+ 	(fn [object & args]		
+ 		(apply object args)
+ 	)
+ )
+
+ (def Triangle 
+ 	(fn [point1 point2 point3]  
+ 		{:point1 point1 
+ 		 :point2 point2
+ 		 :point3 point3
+ 		 :__class_symbol__ 'Triangle }	
+ 	) 
+ )
 
 
--- test/exercises_fp_oo/chapter2_test.clj
+++ test/exercises_fp_oo/chapter2_test.clj
@@ -7,3 +7,14 @@
 	(:x (Point 2 1)) => 2
 	(:y (Point 2 1)) => 1
 )
+
+(fact "make a object Point"
+	(:x (make Point 1 2) ) => 1
+	(class_of (make Point 1 2)) => 'Point
+)
+(fact "make a object Triangle (three Points)"
+	( class_of (make Triangle (make Point 1 2)
+                    (make Point 1 3)
+                    (make Point 3 1))) => 'Triangle
+
+)	
